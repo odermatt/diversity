f9da76438e3d7e5121691ae20b905b34fd371b8d
Daniel <daniel.odermatt@brockmann-consult.de>
2017-01-06 12:54:22 +0100

    Including stacked 1-year time series (internal use only)

--- scripts/plot_timeseries.py
+++ scripts/plot_timeseries.py
@@ -12,7 +12,7 @@ import modules.auxiliary_functions as divaux
 
 
 
-def colors(n_colors):
+def colors():
     # composed with http://tools.medialab.sciences-po.fr/iwanthue/
 
     color_list = ['#bc6559', '#64ac48', '#c35abc', '#9a963f', '#7863cc', '#cb8a41', '#688ccd', '#d34f34',
@@ -93,6 +93,68 @@ def plot_timeseries(x_dates, y_values, y_label, x_min, x_max, y_min, y_max, erro
     plt.close()
 
 
+def stacked_timeseries(x_dates, y_values, y_label, x_min, x_max, y_min, y_max, errors, img_filename, plot_title,
+                       y_axis_label):
+
+    import geoinfo.gicharts as gicharts
+    import pandas as pandas
+
+    marker_size = 10
+    tick_font_size = 12
+    font_style = 'normal'
+    font_family = 'Times New Roman'
+
+    fig = plt.figure(figsize=(15, 7))
+    rect = fig.patch
+    rect.set_facecolor('#ffffff')
+    axes = fig.add_subplot(111)
+
+    font_dict = {'family':font_family, 'style':font_style, 'fontsize':16}
+    axes.set_xlabel('$\mathbf{Date}$', fontdict=font_dict, color='k')
+    axes.set_ylabel(y_axis_label, fontdict=font_dict, color='k')
+
+    font_dict = {'family':font_family, 'style':font_style, 'fontsize':18}
+    axes.set_title('\n' + plot_title, fontdict=font_dict, color='k')
+
+    start_date = x_min
+    end_date = x_max
+
+    # create 11 dates lists, one for each year
+    date_lists = [[] for x in range(11)]
+    data_lists = [[] for x in range(11)]
+
+    for y_ind, year in enumerate(range(2002, 2013)):
+        year_start = datetime.date(year, 1, 1)
+        year_end =   datetime.date(year, 12, 31)
+
+        for d_ind, x_date in enumerate(x_dates):
+
+            if datetime.datetime.date(x_date) >= year_start and datetime.datetime.date(x_date) <= year_end:
+                date_lists[y_ind].append(x_date + datetime.timedelta(days=15))
+                data_lists[y_ind].append(y_values[d_ind])
+
+        if date_lists[y_ind] == []:
+            date_lists[y_ind] = [datetime.date(year, 1, 1)]
+            data_lists[y_ind] = [-1]
+
+    line_colors = colors()
+    marker_colors = colors()
+    dataset_names = list(range(2002, 2013))
+    marker_types = ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+    line_types = ['solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid', 'solid']
+
+    gicharts.timeseries_plot(dates1=date_lists, y1_values=data_lists, dates2=None, y2_values=None,
+                         errors1=None, show_grid=True,
+                         markers=marker_types,
+                         line_styles=line_types,
+                         line_colors=line_colors,
+                         marker_face_colors=marker_colors,
+                         marker_edge_colors=marker_colors,
+                         y1_axis_label=y_axis_label,
+                         y1_labels=dataset_names, x_axis_label='', show_legend=True, twelve_months_scale=True,
+                         img_filename=img_filename, y_min=y_min, y_max=y_max, font_family='times new roman')
+
+
 def plot_dbm_timeseries(param_range, param_str, d2products_folder, lake, stats_str, blacklist):
 
     if d2products_folder == '':
@@ -171,7 +233,9 @@ def plot_dbm_timeseries(param_range, param_str, d2products_folder, lake, stats_s
                     errors=errors, img_filename=dbm_plot_path, plot_title=plot_title, y_axis_label=y_axis_label)
 
 
-def plot_ybm_timeseries(param_range, param_str, d2products_folder, lake, stats_str, blacklist):
+def plot_ybm_timeseries(param_range, param_str, d2products_folder, lake, stats_str, blacklist, stacked=True):
+    #set stacked=False if you prefer to have individual annual plots with error bars
+    #set stacked=True  if you prefer one plot with stacked years
 
     if d2products_folder == '':
         input_file_path = 'Lake-' + lake + '/parameter-stats-monthly/Lake-' + lake + '_' + param_str + '.txt'
@@ -242,13 +306,23 @@ def plot_ybm_timeseries(param_range, param_str, d2products_folder, lake, stats_s
         y_min = divaux.get_range_specs(max(meas_values)*0.5)[0][0]
         y_max = divaux.get_range_specs(max(meas_values)*0.5)[0][1]
 
-    for year in range(2003, 2012):
-        x_min = datetime.date(year - 1, 12, 15)
-        x_max = datetime.date(year, 12, 15)
-        ybm_plot_path = dbm_plot_folder + '/Lake-' + lake + '_' + param_str + '_' + stats_str + '_' + str(year) + '.png'
+    if stacked == False:
+        for year in range(2003, 2012):
+            x_min = datetime.date(year - 1, 12, 15)
+            x_max = datetime.date(year, 12, 15)
+            ybm_plot_path = dbm_plot_folder + '/Lake-' + lake + '_' + param_str + '_' + stats_str + '_' + str(year) + '.png'
 
-        plot_timeseries(meas_dates, meas_values, y_label=y_label, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
-                        errors=errors, img_filename=ybm_plot_path, plot_title=plot_title, y_axis_label=y_axis_label)
+            plot_timeseries(meas_dates, meas_values, y_label=y_label, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
+                            errors=errors, img_filename=ybm_plot_path, plot_title=plot_title, y_axis_label=y_axis_label)
+
+    else:
+        x_min = meas_dates[0]
+        x_max = meas_dates[-1]
+
+        ybm_plot_path = dbm_plot_folder + '/Lake-' + lake + '_' + param_str + '_' + stats_str + '_stacked.png'
+
+        stacked_timeseries(meas_dates, meas_values, y_label=y_label, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max,
+                           errors=errors, img_filename=ybm_plot_path, plot_title=plot_title, y_axis_label=y_axis_label)
 
 
 def plot_dby_timeseries(param_range, param_str, d2products_folder, lake, stats_str, blacklist):
@@ -327,10 +401,13 @@ def main():
     config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../ini', 'plot_timeseries.ini'))
     method = config['DEFAULT']['method']
     d2products_folder = config['DEFAULT']['products_path']
-    param_range = False if config['DEFAULT']['param_range'] == 'False' else config['DEFAULT']['param_range'].split(',')
-    if param_range:
-        param_range = [float(param_range[0]), float(param_range[1])]
     param_str = config['DEFAULT']['param_str']
+    if param_str in ['immersed_cyanobacteria_mean', 'floating_cyanobacteria_mean', 'floating_vegetation_mean']:
+        param_range = [0, 1]
+    else:
+        param_range = False if config['DEFAULT']['param_range'] == 'False' else config['DEFAULT']['param_range'].split(',')
+        if param_range:
+            param_range = [float(param_range[0]), float(param_range[1])]
     lake = config['DEFAULT']['lake']
     stats_str = config['DEFAULT']['stats_str']
     blacklist = config['DEFAULT']['date_blacklist']
@@ -343,6 +420,10 @@ def main():
                             lake=lake, stats_str = stats_str, blacklist=blacklist)
     elif method.lower() == 'decade_by_years':
         plot_dby_timeseries(param_range=param_range, param_str=param_str, d2products_folder=d2products_folder,
+                            lake=lake, stats_str = stats_str, blacklist=blacklist)
+    elif method.lower() == 'years_stacked':
+        import sandbox.daniel.varia.stacked_d24gwa as stacked_d24gwa
+        stacked_d24gwa (param_range=param_range, param_str=param_str, d2products_folder=d2products_folder,
                             lake=lake, stats_str = stats_str, blacklist=blacklist)
     else:
         print('    method not known, please select decade_by_months/year_by_months/decade_by_years')
