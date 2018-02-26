53250b3e598818fe5b822f8a2839b0ad2a997970
Helga Ganz <helgaelisa.ganz@gmail.com>
2018-02-26 16:19:37 +0100

    add new measures to trend

--- scripts/extract_trend.py
+++ scripts/extract_trend.py
@@ -74,13 +74,18 @@ def get_parameter_stats_monthly(param, d2products_folder, lake, config):
 
 
 def calculate_trend(values):
-    coefficients, residuals, _, _, _ = np.polyfit(range(len(values)), values, 1, full=True)
-    if len(residuals) > 0 and residuals[0] != 0:
-        mse = residuals[0] / (len(values))
-        nrmse = np.sqrt(mse) / (max(values) - min(values))
+    if len(values) != 0:
+        coefficients, residuals, _, _, _ = np.polyfit(range(len(values)), values, 1, full=True)
+        if len(residuals) > 0 and residuals[0] != 0:
+            mse = residuals[0] / (len(values))
+            nrmse = np.sqrt(mse) / (max(values) - min(values))
+        else:
+            nrmse = np.nan
+        coeff = coefficients[0]
     else:
+        coeff = np.nan
         nrmse = np.nan
-    return (coefficients[0], nrmse)
+    return (coeff, nrmse)
 
 
 def plots_seasonal_decompose(config, d2products_folder, lakes_list, params_list):
@@ -98,34 +103,37 @@ def plots_seasonal_decompose(config, d2products_folder, lakes_list, params_list)
                 meas_dates, meas_values, errors = divaux.read_statsmonthly(monthly_stats_path, 'average',
                                                                            blacklist)
 
-                # remove leading and trailing NaNs in the time series
-                for i_value, value in enumerate(meas_values):
-                    if not np.isnan(value):
-                        meas_values = meas_values[i_value:]
-                        meas_dates = meas_dates[i_value:]
-                        break
-                for i_value, value in enumerate(reversed(meas_values)):
-                    if not np.isnan(value):
-                        if i_value == 0:
-                            break
-                        else:
-                            meas_values = meas_values[: -i_value]
-                            meas_dates = meas_dates[: -i_value]
-                            break
+                decomposition = seasonal_decompose(meas_dates, meas_values)
+                # fig = decomposition.plot()
+                fig = plot(decomposition, param)
+                fig.savefig(output_folder + '/Lake-' + lake + '_decomposition_' + param + '.png')
 
-                # statsmodels.seasonal_decompose: Plots saved in folder 'decomposition-plots'
-                data = {'date': meas_dates, 'values': meas_values}
-                df = pd.DataFrame(data, columns=['date', 'values'])
-                df.index = df['date']
-                del df['date']
-                # df = df.fillna(method='bfill')
-                # df = df.fillna(method='ffill')
-                # if df.isnull().sum().values[0] == 0:
-                if True:
-                    decomposition = sm.tsa.seasonal_decompose(df, model='additive')
-                    # fig = decomposition.plot()
-                    fig = plot(decomposition, param)
-                    fig.savefig(output_folder + '/Lake-' + lake + '_decomposition_' + param + '.png')
+
+def seasonal_decompose(meas_dates, meas_values):
+    # remove leading and trailing NaNs in the time series
+    for i_value, value in enumerate(meas_values):
+        if not np.isnan(value):
+            meas_values = meas_values[i_value:]
+            meas_dates = meas_dates[i_value:]
+            break
+    for i_value, value in enumerate(reversed(meas_values)):
+        if not np.isnan(value):
+            if i_value == 0:
+                break
+            else:
+                meas_values = meas_values[: -i_value]
+                meas_dates = meas_dates[: -i_value]
+                break
+    # statsmodels.seasonal_decompose: Plots saved in folder 'decomposition-plots'
+    data = {'date': meas_dates, 'values': meas_values}
+    df = pd.DataFrame(data, columns=['date', 'values'])
+    df.index = df['date']
+    del df['date']
+    # df = df.fillna(method='bfill')
+    # df = df.fillna(method='ffill')
+    # if df.isnull().sum().values[0] == 0:
+    decomposition = sm.tsa.seasonal_decompose(df, model='additive')
+    return decomposition
 
 
 def get_blacklist(config, d2products_folder, lake, param):
@@ -147,6 +155,73 @@ def get_blacklist(config, d2products_folder, lake, param):
     return blacklist
 
 
+def add_to_product_stats_10y(config, d2products_folder, lakes_list):
+    for lake in lakes_list:
+        print('Processing lake ' + lake)
+        base_path = d2products_folder + '/Lake-' + lake
+        product_stats_path = base_path + '/product-stats-10y' + '/Lake-' + lake + '_2003-01-01_2011-12-31.txt'
+        if os.path.exists(product_stats_path):
+            table = pandas.read_csv(product_stats_path, sep='\t', header=0, na_values=[''])
+            bands = table['Band']
+            trend = []
+            max_value = []
+            max_month = []
+            min_value = []
+            min_month = []
+            high_residual = []
+            seasonal_variations = []
+            baseline_exceeded = []
+            baselines = []
+            for band in bands:
+                (values, dates) = get_parameter_stats_monthly(band, d2products_folder, lake, config)
+                decomposition = seasonal_decompose(dates, values)
+                # trend
+                idx = np.isfinite(decomposition.trend['values'])
+                (t_seasonal, _) = calculate_trend(decomposition.trend['values'][idx])
+                trend.append(t_seasonal)
+                # min/max month and value seasonal
+                one_year = decomposition.seasonal.loc['20040101':'20041201']
+                max = one_year.max()[0]
+                max_value.append(max)
+                if len(one_year) > 0:
+                    idxmax = one_year.idxmax()[0]
+                else:
+                    idxmax = np.nan
+                max_month.append(idxmax)
+                min = one_year.min()[0]
+                min_value.append(min)
+                if len(one_year) > 0:
+                    idxmin = one_year.idxmin()[0]
+                else:
+                    idxmin = np.nan
+                min_month.append(idxmin)
+
+                # higher than baseline
+                baseline = decomposition.observed.nsmallest(3, 'values').mean()[0]
+                baselines.append(baseline)
+                idx = decomposition.observed['values'] > baseline * 1.5
+                over_baseline = decomposition.observed['values'][idx].index.strftime("%Y-%m-%d").tolist()
+                baseline_exceeded.append(over_baseline)
+
+                # residuum higher than seasonal variation
+                seasonal_variation = abs(max - min)
+                seasonal_variations.append(seasonal_variation)
+                idx = decomposition.resid['values'] > seasonal_variation * 2
+                high = decomposition.resid['values'][idx].index.strftime("%Y-%m-%d").tolist()
+                high_residual.append(high)
+
+            table = table.assign(trend=trend)
+            table = table.assign(max_value_seasonal=max_value)
+            table = table.assign(max_month_seasonal=max_month)
+            table = table.assign(min_value_seasonal=min_value)
+            table = table.assign(min_month_seasonal=min_month)
+            table = table.assign(baseline=baselines)
+            table = table.assign(baseline_exceeded=baseline_exceeded)
+            table = table.assign(seasonal_variations=seasonal_variations)
+            table = table.assign(high_residual=high_residual)
+            table.to_csv(product_stats_path.replace('.txt', '_trend.txt'), '\t')
+
+
 def main():
     config = configparser.ConfigParser()
     config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../ini', 'extract_trend.ini'))
@@ -161,44 +236,8 @@ def main():
     # statsmodels.seasonal_decompose: Plots saved in folder 'decomposition-plots'
     plots_seasonal_decompose(config, d2products_folder, lakes_list, params_list)
 
-    # read product_stats_10y
-    for lake in lakes_list:
-        base_path = d2products_folder + '/Lake-' + lake
-        product_stats_path = base_path + '/product-stats-10y' + '/Lake-' + lake + '_2003-01-01_2011-12-31.txt'
-        if os.path.exists(product_stats_path):
-            table = pandas.read_csv(product_stats_path, sep='\t', header=0, na_values=[''])
-            params = table['Band']
-            trend_monthly_mean = []
-            trend_yearly_mean = []
-            trend_seasonal = []
-            for param in params:
-                (values, dates) = get_parameter_stats_monthly(param, d2products_folder, lake, config)
-                # monthly
-                (t_monthly_mean, _) = calculate_trend(values)
-                trend_monthly_mean.append(t_monthly_mean)
-                # yearly
-                data = {'date': dates, 'values': values}
-                df = pd.DataFrame(data, columns=['date', 'values'])
-                df.index = df['date']
-                del df['date']
-                values_yearly_mean = df.groupby(pd.TimeGrouper('A')).mean()
-                (t_yearly_mean, _) = calculate_trend(values_yearly_mean['values'])
-                trend_yearly_mean.append(t_yearly_mean)
-                # seasonal decomposition
-                df = df.fillna(method='bfill')
-                df = df.fillna(method='ffill')
-                if df.isnull().sum().values[0] == 0:
-                    decomposition = sm.tsa.seasonal_decompose(df, model='additive')
-                    idx = np.isfinite(decomposition.trend['values'])
-                    (t_seasonal, _) = calculate_trend(decomposition.trend['values'][idx])
-                    trend_seasonal.append(t_seasonal)
-                else:
-                    trend_seasonal.append(np.nan)
-
-            table = table.assign(trend_monthly_mean=trend_monthly_mean)
-            table = table.assign(trend_yearly_mean=trend_yearly_mean)
-            table = table.assign(trend_seasonal_decomposition=trend_seasonal)
-            table.to_csv(product_stats_path + '_trend.csv', '\t')
+    # add info to product_stats_10y for all parameters
+    add_to_product_stats_10y(config, d2products_folder, lakes_list)
 
 
 if __name__ == "__main__":
